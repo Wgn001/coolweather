dd5bd7b93de88a2838b997caac9c0479d3aeeb14
Gn <1281555629@qq.com>
2019-02-28 01:29:29 +0800

    完成遍历省市县三级列表的功能

--- app/src/main/java/gn/example/coolweather/ui/MainActivity.java
+++ app/src/main/java/gn/example/coolweather/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package gn.example.coolweather;
+package gn.example.coolweather.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import gn.example.coolweather.R;
 
 public class MainActivity extends AppCompatActivity {
 
